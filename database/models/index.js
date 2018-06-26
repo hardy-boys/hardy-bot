3792b4463a7f01e9654b12d8c0f4dfb07b5aa02c
mattl <matt.lee.personal@gmail.com>
2018-06-26 17:04:09 -0500

    added db server connection, db profile save

--- database/models/index.js
+++ database/models/index.js
@@ -4,40 +4,40 @@ const Sequelize = require('sequelize');
 
 const { Op } = Sequelize;
 
-//setup connection to server
-const sequelize = new Sequelize('thesis', 'hardyboys', null, {
-  host: 'localhost',
-  port: '5432',
+// setup connection to server
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: 'postgres',
   operatorsAliases: false,
   logging: false,
-//   dialectOptions: {
-//     ssl: true,
-//   },
+  dialectOptions: {
+    ssl: true,
+  },
 });
 
-//testing connection
+// testing connection
 sequelize.authenticate()
-    .then(() => {
-        console.log('Connected DB to server.');
-    })
-    .catch((error) => {
-        console.log('Unable to connect to the DB:', error);
-    });
+  .then(() => {
+    console.log('Connected DB to server.');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to the DB:', error);
+  });
 
-//model object created to import all DB tables
+// model object created to import all DB tables
 const models = {
-    User: sequelize.import('./user'),
-    Device: sequelize.import('./devices'),
-    Profile: sequelize.import('./profiles'),
-    Widget: sequelize.import('./widgets'),
-}
+  User: sequelize.import('./user'),
+  Device: sequelize.import('./devices'),
+  Profile: sequelize.import('./profiles'),
+  Widget: sequelize.import('./widgets'),
+};
 
-//create relationships between all tables with associations 
+// create relationships between all tables with associations
 Object.keys(models).forEach((modelName) => {
-    if('associate' in models[modelName]) {
-        models[modelName].associate(models);
-    }
+  if ('associate' in models[modelName]) {
+    models[modelName].associate(models);
+  }
 });
 
 models.sequelize = sequelize;
--- database/models/widgets.js
+++ database/models/widgets.js
@@ -1,10 +1,14 @@
 module.exports = (sequelize, DataTypes) => {
   const Widget = sequelize.define('widget', {
-    apiKey: {
+    name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
     widgetInfo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    apiKey: {
       type: DataTypes.STRING,
       allowNull: false,
     },
