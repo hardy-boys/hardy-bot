4f24aef9c2e64ca0704d22604a71a5a0c194e265
Raphael Croce <rcroce7@gmail.com>
2018-06-27 16:57:57 -0500

    Refactor store to include middleware

--- react-client/src/store/index.js
+++ react-client/src/store/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { applyMiddleware, createStore, combineReducers } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
 import logger from 'redux-logger';
 
-import reducers from '../reducers/index';
+import reducers from '../reducers';
 
 const middleware = applyMiddleware(logger);
 
 const store = createStore(
-  combineReducers({
-    state: reducers,
-  }),
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  middleware,
+  reducers,
+  compose(
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    middleware,
+  ),
 );
 
 export default store;
