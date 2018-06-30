b06a0717f812b37b030feab297790df7f07539ed
Raphael Croce <rcroce7@gmail.com>
2018-06-29 19:08:01 -0500

    Add stocks reducer to combined reducers

--- react-client/src/reducers/index.js
+++ react-client/src/reducers/index.js
@@ -1,9 +1,12 @@
 import { combineReducers } from 'redux';
 
 import WeatherReducer from './weather';
+import StocksReducer from './stocks';
 
 const rootReducer = combineReducers({
   weather: WeatherReducer,
+  stocks: StocksReducer,
+
 });
 
 export default rootReducer;
