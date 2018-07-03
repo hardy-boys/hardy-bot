5adbecf9ec4d5f4c4b8b6bbd53fcbeaf3646130e
Raphael Croce <rcroce7@gmail.com>
2018-07-03 01:15:25 -0500

    Refactor weather widget to dynamically render weather data

--- react-client/src/views/Widgets/WeatherWidget.jsx
+++ react-client/src/views/Widgets/WeatherWidget.jsx
@@ -1,65 +1,71 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators, compose } from 'redux';
+import { compose } from 'redux';
 
 class WeatherWidget extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
-    return (
-    <div style={{
-width: '550px', opacity: '.75', float: 'right', backgroundImage: 'url("https://png.pngtree.com/thumb_back/fh260/back_pic/03/71/82/5157b849c6299b1.jpg")',
-}}>
+    if (this.props.weather.fetched) {
+      let { temp, pressure, humidity } = this.props.weather.weatherData.main;
+      let { name, wind } = this.props.weather.weatherData;
+      return (
         <div style={{
-width: '50%', float: 'left', minHeight: '150px', position: 'relative',
-}}>
-        <span style={{ position: 'absolute', bottom: '0' }}><h1 style={{ color: 'white', fontSize: '72px', margin: '10px' }}><strong>86°</strong></h1></span>
+            width: '550px', opacity: '.75', float: 'right',
+        }}>
+            <div style={{
+                width: '50%', float: 'left', minHeight: '150px', position: 'relative',
+            }}>
+                <span style={{ position: 'absolute', bottom: '0' }}><h1 style={{ color: 'black', fontSize: '72px', margin: '10px' }}><strong>{temp}°</strong></h1></span>
+            </div>
+            <div style={{ width: '50%', float: 'right' }}>
+                <div style={{
+                    textAlign: 'right', padding: '10px', color: 'black', fontSize: '36px',
+                }}>{name}</div>
+                <div style={{ textAlign: 'right', minHeight: '106px', padding: '0px 10px' }}>
+                    <p style={{ margin: '0px', color: 'black' }}>HUMIDITY: <strong>{humidity}%</strong></p>
+                    <p style={{ margin: '0px', color: 'black' }}>WIND: <strong>{wind.speed}MPH</strong></p>
+                </div>
+            </div>
+            <div style={{
+                width: '100%', display: 'table', tableLayout: 'fixed', minHeight: '50px',
+            }}>
+                <div style={{ display: 'table-cell', textAlign: 'center', color: 'black' }}>
+                    SUN
+        <p><strong>87°</strong></p>
+                </div>
+                <div style={{ display: 'table-cell', textAlign: 'center', color: 'black' }}>
+                    MON
+        <p><strong>92°</strong></p>
+                </div>
+                <div style={{ display: 'table-cell', textAlign: 'center', color: 'black' }}>
+                    TUE
+        <p><strong>93°</strong></p>
+                </div>
+                <div style={{ display: 'table-cell', textAlign: 'center', color: 'black' }}>
+                    WED
+        <p><strong>99°</strong></p>
+                </div>
+                <div style={{ display: 'table-cell', textAlign: 'center', color: 'black' }}>
+                    THU
+        <p><strong>89°</strong></p>
+                </div>
+                <div style={{ display: 'table-cell', textAlign: 'center', color: 'black' }}>
+                    FRI
+        <p><strong>92°</strong></p>
+                </div>
+                <div style={{ display: 'table-cell', textAlign: 'center', color: 'black' }}>
+                    SAT
+        <p><strong>101°</strong></p>
+                </div>
+            </div>
         </div>
-        <div style={{ width: '50%', float: 'right' }}>
-          <div style={{
-textAlign: 'right', padding: '10px', color: 'white', fontSize: '36px',
-}}>AUSTIN, TX</div>
-          <div style={{ textAlign: 'right', minHeight: '106px', padding: '0px 10px' }}>
-              <p style={{ margin: '0px', color: 'white' }}>HUMIDITY: <strong>50%</strong></p>
-              <p style={{ margin: '0px', color: 'white' }}>WIND: <strong>10MPH E</strong></p>
-          </div>
-        </div>
-        <div style={{
-width: '100%', display: 'table', tableLayout: 'fixed', minHeight: '50px',
-}}>
-          <div style={{ display: 'table-cell', textAlign: 'center', color: 'white' }}>
-              SUN
-              <p><strong>87°</strong></p>
-          </div>
-          <div style={{ display: 'table-cell', textAlign: 'center', color: 'white' }}>
-              MON
-              <p><strong>92°</strong></p>
-              </div>
-          <div style={{ display: 'table-cell', textAlign: 'center', color: 'white' }}>
-              TUE
-              <p><strong>93°</strong></p>
-              </div>
-          <div style={{ display: 'table-cell', textAlign: 'center', color: 'white' }}>
-              WED
-              <p><strong>99°</strong></p>
-              </div>
-          <div style={{ display: 'table-cell', textAlign: 'center', color: 'white' }}>
-              THU
-              <p><strong>89°</strong></p>
-              </div>
-          <div style={{ display: 'table-cell', textAlign: 'center', color: 'white' }}>
-              FRI
-              <p><strong>92°</strong></p>
-              </div>
-          <div style={{ display: 'table-cell', textAlign: 'center', color: 'white' }}>
-              SAT
-              <p><strong>101°</strong></p>
-              </div>
-        </div>
-    </div>
-    );
+      );
+    } else {
+      return (
+            <div>
+                <p>Loading...</p>
+            </div>
+      );
+    }
   }
 }
 
@@ -67,6 +73,4 @@ const mapStateToProps = (state) => {
   return { weather: state.weather };
 };
 
-export default compose(connect(mapStateToProps),
-  // connect(null, mapDispatchToProps),
-)(WeatherWidget);
+export default compose(connect(mapStateToProps))(WeatherWidget);
