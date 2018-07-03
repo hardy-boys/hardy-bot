000a1357fc9eab077e0e3302d00ca96779ca2c62
Raphael Croce <rcroce7@gmail.com>
2018-07-03 01:16:55 -0500

    Add devices reducer

--- react-client/src/reducers/index.js
+++ react-client/src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import WeatherReducer from './weather';
 import StocksReducer from './stocks';
+import DevicesReducer from './devices';
 
 const rootReducer = combineReducers({
   weather: WeatherReducer,
   stocks: StocksReducer,
+  userDevices: DevicesReducer,
 
 });
 
