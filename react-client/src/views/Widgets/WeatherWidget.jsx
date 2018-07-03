c4623b11520670caaf0cc48c6115e130361dd72b
Raphael Croce <rcroce7@gmail.com>
2018-07-03 17:05:39 -0500

    Implement weather widget modal to allow users to edit the zipcode

--- react-client/src/views/Widgets/WeatherWidget.jsx
+++ react-client/src/views/Widgets/WeatherWidget.jsx
@@ -2,7 +2,23 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 
+import Button from 'components/CustomButtons/Button.jsx';
+import WeatherWidgetModal from './WeatherWidgetModal';
+
 class WeatherWidget extends React.Component {
+  // Temporary state until it gets refactored to Redux
+  state = {
+    open: false,
+  };
+
+  handleOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
   render() {
     if (this.props.weather.fetched) {
       let { temp, pressure, humidity } = this.props.weather.weatherData.main;
@@ -11,59 +27,61 @@ class WeatherWidget extends React.Component {
         <div style={{
             width: '550px', opacity: '.75', float: 'right',
         }}>
+          <div style={{
+              width: '50%', float: 'left', minHeight: '150px', position: 'relative',
+          }}>
+            <span style={{ position: 'absolute', bottom: '0' }}><h1 style={{ color: 'black', fontSize: '72px', margin: '10px' }}><strong>{temp}°</strong></h1></span>
+          </div>
+          <div style={{ width: '50%', float: 'right' }}>
             <div style={{
-                width: '50%', float: 'left', minHeight: '150px', position: 'relative',
-            }}>
-                <span style={{ position: 'absolute', bottom: '0' }}><h1 style={{ color: 'black', fontSize: '72px', margin: '10px' }}><strong>{temp}°</strong></h1></span>
-            </div>
-            <div style={{ width: '50%', float: 'right' }}>
-                <div style={{
-                    textAlign: 'right', padding: '10px', color: 'black', fontSize: '36px',
-                }}>{name}</div>
-                <div style={{ textAlign: 'right', minHeight: '106px', padding: '0px 10px' }}>
-                    <p style={{ margin: '0px', color: 'black' }}>HUMIDITY: <strong>{humidity}%</strong></p>
-                    <p style={{ margin: '0px', color: 'black' }}>WIND: <strong>{wind.speed}MPH</strong></p>
-                </div>
+                textAlign: 'right', padding: '10px', color: 'black', fontSize: '36px',
+            }}>{name}</div>
+              <div style={{ textAlign: 'right', minHeight: '106px', padding: '0px 10px' }}>
+                <p style={{ margin: '0px', color: 'black' }}>HUMIDITY: <strong>{humidity}%</strong></p>
+                <p style={{ margin: '0px', color: 'black' }}>WIND: <strong>{wind.speed}MPH</strong></p>
+              </div>
             </div>
             <div style={{
                 width: '100%', display: 'table', tableLayout: 'fixed', minHeight: '50px',
             }}>
-                <div style={{ display: 'table-cell', textAlign: 'center', color: 'black' }}>
-                    SUN
-        <p><strong>87°</strong></p>
-                </div>
-                <div style={{ display: 'table-cell', textAlign: 'center', color: 'black' }}>
-                    MON
-        <p><strong>92°</strong></p>
-                </div>
-                <div style={{ display: 'table-cell', textAlign: 'center', color: 'black' }}>
-                    TUE
-        <p><strong>93°</strong></p>
-                </div>
-                <div style={{ display: 'table-cell', textAlign: 'center', color: 'black' }}>
-                    WED
-        <p><strong>99°</strong></p>
-                </div>
-                <div style={{ display: 'table-cell', textAlign: 'center', color: 'black' }}>
-                    THU
-        <p><strong>89°</strong></p>
-                </div>
-                <div style={{ display: 'table-cell', textAlign: 'center', color: 'black' }}>
-                    FRI
-        <p><strong>92°</strong></p>
-                </div>
-                <div style={{ display: 'table-cell', textAlign: 'center', color: 'black' }}>
-                    SAT
-        <p><strong>101°</strong></p>
-                </div>
+              <div style={{ display: 'table-cell', textAlign: 'center', color: 'black' }}>
+                SUN
+                <p><strong>87°</strong></p>
+              </div>
+              <div style={{ display: 'table-cell', textAlign: 'center', color: 'black' }}>
+                 MON
+                <p><strong>92°</strong></p>
+              </div>
+              <div style={{ display: 'table-cell', textAlign: 'center', color: 'black' }}>
+                TUE
+                <p><strong>93°</strong></p>
+              </div>
+              <div style={{ display: 'table-cell', textAlign: 'center', color: 'black' }}>
+                WED
+                <p><strong>99°</strong></p>
+              </div>
+              <div style={{ display: 'table-cell', textAlign: 'center', color: 'black' }}>
+                THU
+                <p><strong>89°</strong></p>
+              </div>
+              <div style={{ display: 'table-cell', textAlign: 'center', color: 'black' }}>
+                FRI
+                <p><strong>92°</strong></p>
+              </div>
+              <div style={{ display: 'table-cell', textAlign: 'center', color: 'black' }}>
+                SAT
+                <p><strong>101°</strong></p>
+              </div>
             </div>
+          <Button onClick={this.handleOpen.bind(this)} color="primary">Edit Widget</Button>
+          <WeatherWidgetModal open={this.state.open} close={this.handleClose.bind(this)}/>
         </div>
       );
     } else {
       return (
-            <div>
-                <p>Loading...</p>
-            </div>
+        <div>
+          <p>Loading...</p>
+        </div>
       );
     }
   }
