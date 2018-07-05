1383ca9f1e5d39dc7034ad0bf70e9c71b0d2dc94
Raphael Croce <rcroce7@gmail.com>
2018-07-05 14:24:38 -0500

    Add post stock route to fetch new stock quote when a user configures widget

--- server/routes/stocks.js
+++ server/routes/stocks.js
@@ -71,7 +71,7 @@ router.get('/api/stocks', (req, res) => {
       // do whatever you wish with the update data
       console.log(result);
       // pushToDevice(mapData(symbol, result), req.session.particleToken);
-      const data = result[0];
+      const data = result;
       io.emit('action', { type: actions.STOCK_DATA_UPDATE, data: { data } });
     })
 
@@ -91,6 +91,61 @@ router.get('/api/stocks', (req, res) => {
 router.get('/api/stocks/close', (req, res) => {
   eventSource.close();
   res.status(200).end('Stock polling stopped');
+});
+
+router.post('/api/stocks', (req, res) => {
+  eventSource.close();
+  let { symbol } = req.body;
+
+  let targetUrl = `https://api.iextrading.com/1.0/stock/${symbol}/batch?types=quote`;
+
+  eventSource = streamdataio.createEventSource(targetUrl, appToken);
+
+  let result;
+  const io = req.app.get('socketio');
+
+  eventSource
+    // the standard 'open' callback will be called when connection is established with the server
+    .onOpen(() => {
+      console.log('connected!');
+    })
+    // the streamdata.io specific 'data' event will be called when a fresh Json data set
+    // is pushed by Streamdata.io coming from the API
+    .onData((data) => {
+      console.log('data received');
+      // memorize the fresh data set
+      result = data;
+      // io.emit('action', { type: actions.WEATHER_DATA_RECEIVED, data: result });
+      console.log(result);
+      // pushToDevice(mapData(result), req.session.particleToken);
+      res.send(result);
+    })
+    // the streamdata.io specific 'patch' event will be called when a fresh Json patch
+    // is pushed by streamdata.io from the API. This patch has to be applied to the
+    // latest data set provided.
+    .onPatch((patch) => {
+      // display the patch
+      console.log('patch: ', patch);
+      // apply the patch to data using json patch API
+      jsonPatch.applyPatch(result, patch);
+      console.log('RESULT', result);
+      // do whatever you wish with the update data
+      // console.log(result);
+      io.emit('action', { type: actions.STOCK_DATA_UPDATE, data: result });
+      // pushToDevice(mapData(result), req.session.particleToken);
+    })
+
+    // the standard 'error' callback will be called when an error occur with the evenSource
+    // for example with an invalid token provided
+    .onError((error) => {
+      console.log('ERROR!', error);
+      eventSource.close();
+      io.emit('action', { type: actions.STOCK_REQUEST_ERROR, data: error });
+    });
+
+  eventSource.open();
+
+  // res.status(200).end('Weather polling started');
 });
 
 module.exports = router;
