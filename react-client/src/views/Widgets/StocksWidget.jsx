16d1c0301ffed11a2fc93bee63c94605143ca09c
Raphael Croce <rcroce7@gmail.com>
2018-07-05 14:21:49 -0500

    Implement logic for opening and closing stock widget modal

--- react-client/src/views/Widgets/StocksWidget.jsx
+++ react-client/src/views/Widgets/StocksWidget.jsx
@@ -3,8 +3,21 @@ import { connect } from 'react-redux';
 import { compose } from 'redux';
 import Button from 'components/CustomButtons/Button.jsx';
 import Table from 'components/Table/Table.jsx';
+import StocksWidgetModal from './StocksWidgetModal';
 
 class StocksWidget extends React.Component {
+  state = {
+    open: false,
+  };
+
+  handleOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
   render() {
     // console.log('STOCK PROPS', this.props.stocks);
     let stockBatch = this.props.stocks.stocksData.data;
@@ -28,7 +41,8 @@ class StocksWidget extends React.Component {
             tableHead={['Symbol', 'Price', 'Change', 'Price Chart']}
             tableData={stockData}
           />
-          <Button color="primary">Edit Widget</Button>
+          <Button onClick={this.handleOpen.bind(this)} color="primary">Edit Widget</Button>
+          <StocksWidgetModal open={this.state.open} close={this.handleClose.bind(this)}/>
         </div>
       );
     } else {
