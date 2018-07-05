4b7e541d18d3fd309cb85e409d546cacdf79dbbe
Raphael Croce <rcroce7@gmail.com>
2018-07-05 10:04:18 -0500

    Add fake stock data and inject into initial state

--- react-client/src/reducers/stocks.js
+++ react-client/src/reducers/stocks.js
@@ -1,9 +1,44 @@
 import { STOCK_REQUEST_RECEIVED, STOCK_REQUEST_ERROR, STOCK_DATA_RECEIVED, STOCK_DATA_UPDATE } from '../actions/types';
 
+const fakeStockData = {
+  AAPL: {
+    quote: {
+      symbol: 'AAPL',
+      companyName: 'Apple Inc.',
+      latestPrice: 183.92,
+      change: -3.26,
+    },
+  },
+  FB: {
+    quote: {
+      symbol: 'FB',
+      companyName: 'Facebook Inc.',
+      latestPrice: 192.73,
+      change: 0,
+    },
+  },
+  GOOGL: {
+    quote: {
+      symbol: 'GOOGL',
+      companyName: 'Alphabet Inc.',
+      latestPrice: 1116.28,
+      change: 0,
+    },
+  },
+  AMZN: {
+    quote: {
+      symbol: 'AMZN',
+      companyName: 'Amazon.com Inc.',
+      latestPrice: 1693.96,
+      change: -19.82,
+    },
+  },
+};
+
 const initialState = {
   fetching: false,
   fetched: false,
-  stockData: {},
+  stocksData: { data: fakeStockData },
   error: null,
 };
 
@@ -18,12 +53,12 @@ export default (state = initialState, action) => {
         ...state,
         fetched: true,
         fetching: false,
-        stockData: action.data,
+        stocksData: action.data,
       };
     case STOCK_DATA_UPDATE:
       return {
         ...state,
-        stockData: action.data,
+        stocksData: action.data,
       };
     default:
       return state;
