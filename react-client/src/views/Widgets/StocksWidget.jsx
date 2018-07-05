4e30e05c921bb0bbf6075cd430e1c2305b778834
Raphael Croce <rcroce7@gmail.com>
2018-07-05 10:05:43 -0500

    Format stock data to use in table

--- react-client/src/views/Widgets/StocksWidget.jsx
+++ react-client/src/views/Widgets/StocksWidget.jsx
@@ -1,15 +1,34 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
+import Button from 'components/CustomButtons/Button.jsx';
+import Table from 'components/Table/Table.jsx';
 
 class StocksWidget extends React.Component {
   render() {
+    // console.log('STOCK PROPS', this.props.stocks);
+    let stockBatch = this.props.stocks.stocksData.data;
+    let stockSymbols = Object.keys(stockBatch);
+    let stockData = [];
+    // console.log('STOCK BATCH', stockBatch);
+    stockSymbols.forEach((symbol, index) => {
+      if (Object.prototype.hasOwnProperty.call(stockBatch, symbol)) {
+        let stockArr = [symbol];
+        const { latestPrice, change } = stockBatch[symbol].quote;
+        stockArr.push(`$ ${latestPrice}`, `${change}`);
+        stockData.push(stockArr);
+      }
+    });
+
     if (this.props.stocks.fetched) {
-      let { symbol, latestPrice } = this.props.stocks.stockData.data;
       return (
         <div>
-          <p> Stock: {symbol} </p>
-          <p> Current Price: {latestPrice} </p>
+          <Table
+            tableHeaderColor="primary"
+            tableHead={['Symbol', 'Price', 'Change', 'Price Chart']}
+            tableData={stockData}
+          />
+          <Button color="primary">Edit Widget</Button>
         </div>
       );
     } else {
