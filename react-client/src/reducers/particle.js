1047a34d3d45814ad023a6369d1155d0daf66e5b
Raphael Croce <rcroce7@gmail.com>
2018-07-10 00:40:51 -0500

    Add device action types and implement reducers to handle such actions and state rendering

--- react-client/src/reducers/particle.js
+++ react-client/src/reducers/particle.js
@@ -2,12 +2,21 @@ import {
   SIGN_IN_TO_PARTICLE,
   PARTICLE_SIGN_IN_SUCCESSFUL,
   PARTICLE_SIGN_IN_ERROR,
+
+  GETTING_DEVICE_INFO,
+  DEVICE_INFO_RECEIVED,
+  ERROR_RETRIEVING_DEVICE_INFO,
+  DEVICE_STATUS_UPDATE,
+  DEVICE_DIAGNOSTICS_UPDATE,
 } from '../actions/types';
 
 const initialState = {
   devices: [],
+  deviceStats: [],
   signingInToParticle: false,
   signedInToParticle: false,
+  gettingDeviceInfo: false,
+  deviceInfoReceived: false,
   error: null,
 };
 
@@ -29,6 +38,36 @@ export default (state = initialState, action) => {
       return {
         ...state,
         signingInToParticle: false,
+        error: action.payload,
+      };
+    case GETTING_DEVICE_INFO:
+      return {
+        ...state,
+        gettingDeviceInfo: true,
+      };
+    case DEVICE_INFO_RECEIVED:
+      return {
+        ...state,
+        gettingDeviceInfo: false,
+        deviceInfoReceived: true,
+        deviceStats: action.payload,
+      };
+    case ERROR_RETRIEVING_DEVICE_INFO:
+      return {
+        ...state,
+        gettingDeviceInfo: false,
+        error: action.payload,
+      };
+    case DEVICE_STATUS_UPDATE:
+      return {
+        ...state,
+        gettingDeviceInfo: false,
+        error: action.payload,
+      };
+    case DEVICE_DIAGNOSTICS_UPDATE:
+      return {
+        ...state,
+        gettingDeviceInfo: false,
         error: action.payload,
       };
     default:
