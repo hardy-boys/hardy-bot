98e4d292e3a6787a71a771f475b37c25d1378e54
Raphael Croce <rcroce7@gmail.com>
2018-07-10 00:40:17 -0500

    Refactor sign in to particle action creator to get device stats if succesfully signed in

--- react-client/src/actions/particle.js
+++ react-client/src/actions/particle.js
@@ -4,6 +4,10 @@ import {
   SIGN_IN_TO_PARTICLE,
   PARTICLE_SIGN_IN_SUCCESSFUL,
   PARTICLE_SIGN_IN_ERROR,
+
+  GETTING_DEVICE_INFO,
+  DEVICE_INFO_RECEIVED,
+  ERROR_RETRIEVING_DEVICE_INFO,
 } from './types';
 
 const signInToParticle = () => {
@@ -12,7 +16,19 @@ const signInToParticle = () => {
     axios.get('/particle/login')
       .then((res) => {
         // console.log('SIGN IN ACTION RES', res.data);
+        const deviceName = res.data[0].name;
+
         dispatch({ type: PARTICLE_SIGN_IN_SUCCESSFUL, payload: res.data });
+        dispatch({ type: GETTING_DEVICE_INFO });
+        axios.post('/particle/stats', { deviceName })
+          .then((deviceInfo) => {
+            let deviceStats = deviceInfo.data;
+            // console.log('DEVICE INFO RECEIVED', deviceInfo.data);
+            dispatch({ type: DEVICE_INFO_RECEIVED, payload: { deviceName, deviceStats } });
+          })
+          .catch((err) => {
+            dispatch({ type: ERROR_RETRIEVING_DEVICE_INFO, payload: err });
+          });
       })
       .catch((err) => {
         // console.log('SIGN IN ACTION ERR', err);
