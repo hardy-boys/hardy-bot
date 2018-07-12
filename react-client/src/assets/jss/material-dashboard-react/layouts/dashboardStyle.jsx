0dcb1a6b22019457e148a2c36dbb6b38fd19951c
Trevor Sasaki <Trevor_sasaki@hotmail.comgit config --global user.email Trevor_sasaki@hotmail.comgit config --global core.autocrlf inputgit config --global core.safecrlf true>
2018-07-12 09:14:51 -0500

    took headers out of dashboard views removed margin

--- react-client/src/assets/jss/material-dashboard-react/layouts/dashboardStyle.jsx
+++ react-client/src/assets/jss/material-dashboard-react/layouts/dashboardStyle.jsx
@@ -23,8 +23,8 @@ const appStyle = theme => ({
     overflowScrolling: "touch"
   },
   content: {
-    marginTop: "70px",
-    padding: "30px 15px",
+    marginTop: "5px",
+    padding: "5px 15px",
     minHeight: "calc(100vh - 123px)"
   },
   container,
--- react-client/src/layouts/Dashboard/Dashboard.jsx
+++ react-client/src/layouts/Dashboard/Dashboard.jsx
@@ -66,11 +66,11 @@ class App extends React.Component {
           {...rest}
         />
         <div className={classes.mainPanel} ref="mainPanel">
-          <Header
+          {/* <Header
             routes={dashboardRoutes}
             handleDrawerToggle={this.handleDrawerToggle}
             {...rest}
-          />
+          /> */}
           {/* On the /maps route we want the map to be on full screen - this is not possible if the content and conatiner classes are present because they have some paddings which would make the map smaller */}
           {this.getRoute() ? (
             <div className={classes.content}>
